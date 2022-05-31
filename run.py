e867bc69549544bc775fc315108c3a842169e36e
Broso56 <Broso564@gmail.com>
2022-05-30 21:20:28 -0700

    made reload command have options for all commands rather than typing file name

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from clienttoken import token # Client token. NEVER share this with anyone, as it gives them access to your bot.
 import discord                      # ^ Changed file name from 'bottoken' to 'clienttoken' locally to avoid my token file from being tracked + committed
 from discord.ext import commands
+from discord import app_commands
+from discord.app_commands import Choice
 import datetime
 import pytz
 import asyncio
@@ -9,6 +11,7 @@ intents = discord.Intents.default() # Required intent stuff
 intents.members = True
 intents.message_content = True
 client = commands.Bot(command_prefix="^", help_command=None, case_insensitive=True, intents=intents.all())
+tree = app_commands
 
 
 @client.event
@@ -19,10 +22,17 @@ async def on_ready():
     print(f"READY: Bot readied at {current_time}")
 
 @client.tree.command(name='reload')
+@tree.choices(
+    command=[
+            Choice(name='Profile', value='profile'),
+            Choice(name='Map', value='map'),
+            Choice(name='Sync', value='sync')
+        ]
+)
 @commands.is_owner()
-async def reload(interaction: discord.Interaction, command: str):
-    await client.reload_extension(f"{command}")
-    await interaction.response.send_message(f'{command} reloaded!', ephemeral=True)
+async def reload(interaction: discord.Interaction, command: Choice[str]):
+    await client.reload_extension(f"cmd{command.value}")
+    await interaction.response.send_message(f'{command.name} command reloaded!', ephemeral=True)
 
 async def setup(): # Loading cogs from other files
 
